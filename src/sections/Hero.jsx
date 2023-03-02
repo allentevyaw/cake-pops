793faeec7f02d544aea47b89861fdd1030f9c491
allentevyaw <atevyaw@gmail.com>
2023-03-01 20:17:08 -0500

    hero changes

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,6 +3,8 @@ import {motion} from 'framer-motion'
 import { slideIn, staggerContainer, textVariant } from '../../utils/motion';
 import styles from '../styles';
 import cake from '../assets/cake.png'
+import {CiLollipop} from 'react-icons/ci'
+
 
 
 const Hero = () => {
@@ -41,14 +43,26 @@ const Hero = () => {
         variants={slideIn('right', 'tween', 0.1, .8)}
         className="mt-[30px] flex justify-center"
       >
+        <div className='flex flex-col items-center'>
         <img
           src={cake}
           alt="hero_cover"
-          className="w-[50%] md:w-[40%] sm:h-[500px] h-[350px] object-cover 
+          className="sm:h-[400px] md:h-[450px] lg:h-[500px] h-[350px] object-cover 
           rounded-tl-[140px] rounded-tr-[140px] rounded-br-[140px] 
           rounded-bl-[140px] z-10 relative pb-7"
         />
+
+
+      <button className='bg-gradient-to-r from-purple-300 to-[#F40076] font-semibold text-white 
+      rounded-lg mb-10 p-3 shadow-lg hover:opacity-80'>
+        LET'S POP!
+      </button>
+      </div>
+
+
         </motion.div>
+
+
       </motion.div>
     </section>
   )
