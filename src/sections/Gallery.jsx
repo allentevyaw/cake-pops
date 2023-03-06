a01dc0baa37cd1e28a5f22a9dba6f0a09a98f9f3
allentevyaw <atevyaw@gmail.com>
2023-03-05 19:42:13 -0500

    gallary

--- src/sections/Gallery.jsx
+++ src/sections/Gallery.jsx
@@ -7,15 +7,53 @@ import sprinkle from '../assets/sprinkle.jpg'
 
 const Gallery = () => {
   return (
-    <div>
-        <div className='grid grid-cols-2 px-5 w-full h-screen bg-pink-400'>
 
-        <img src={oreo} alt='oreo' className='w-[400px] h-[400px]'/>
-        <img src={carrot} alt='carrot' className='w-[400px] h-[400px]' />
-        <img src={carrot2} alt='carrot2' className='w-[400px] h-[400px]' />
-        <img src={sprinkle} alt='sprinkle' className='w-[400px] h-[400px]' />
+<section class="overflow-hidden text-neutral-700">
+  <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-24">
+    <div class="-m-1 flex flex-wrap md:-m-2">
+      <div class="flex w-1/2 flex-wrap">
+        <div class="w-1/2 p-1 md:p-2">
+          <img
+            alt="oreo"
+            class="block h-full w-full rounded-lg object-cover object-center"
+            src={oreo} />
         </div>
+        <div class="w-1/2 p-1 md:p-2">
+          <img
+            alt="sprinkle"
+            class="block h-full w-full rounded-lg object-cover object-center"
+            src={sprinkle} />
+        </div>
+        <div class="w-full p-1 md:p-2">
+          <img
+            alt="carrot"
+            class="block h-full w-full rounded-lg object-cover object-center"
+            src={carrot} />
+        </div>
+      </div>
+      <div class="flex w-1/2 flex-wrap">
+        <div class="w-full p-1 md:p-2">
+          <img
+            alt="carrot2"
+            class="block h-full w-full rounded-lg object-cover object-center"
+            src={carrot2} />
+        </div>
+        <div class="w-1/2 p-1 md:p-2">
+          <img
+            alt="gallery"
+            class="block h-full w-full rounded-lg object-cover object-center"
+            src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp" />
+        </div>
+        <div class="w-1/2 p-1 md:p-2">
+          <img
+            alt="gallery"
+            class="block h-full w-full rounded-lg object-cover object-center"
+            src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp" />
+        </div>
+      </div>
     </div>
+  </div>
+</section>
   )
 }
 
