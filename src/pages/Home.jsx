c3b3c550a088b47bbba6a57f9f7f48d921011d3b
allentevyaw <atevyaw@gmail.com>
2023-03-09 17:32:58 -0500

    fixed src bug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import Hero from '../sections/Hero'
-import Navbar from '../sections/Navbar'
 import Social from '../sections/Social'
 import About from "../sections/About"
-import Photos from '../sections/photos'
 
 
 const Home = () => {
